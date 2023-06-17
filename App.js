40f0766af83a022eff91cef2b50162ecf7ae72f0
Allan Blackmar <allan.blackmar@gmail.com>
2023-06-17 16:18:27 -0400

    habitz-4: add icons for navigation tabs

--- App.js
+++ App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import HabitTrackerScreen from './src/screens/HabitTrackerScreen';
+import Ionicons, { IoniconsGlyphs } from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Habit Tracker') {
+                            iconName = focused ? 'checkmark-circle' : 'checkmark-circle-outline';
+                        } else if (route.name === 'test 2') {
+                            iconName = focused ? 'chatbubbles-sharp' : 'chatbubbles-sharp';
+                        } else if (route.name === 'test 3') {
+                            iconName = focused ? 'chatbubbles-sharp' : 'chatbubbles-sharp';
+                        } else if (route.name === 'test 4') {
+                            iconName = focused ? 'chatbubbles-sharp' : 'chatbubbles-sharp';
+                        }
+
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                })}
+            >
                 <Tab.Screen name="Habit Tracker" component={HabitTrackerScreen} />
+                <Tab.Screen name="test 2" component={HabitTrackerScreen} />
+                <Tab.Screen name="test 3" component={HabitTrackerScreen} />
+                <Tab.Screen name="test 4" component={HabitTrackerScreen} />
                 {/* Add more screens and icons as needed */}
             </Tab.Navigator>
         </NavigationContainer>
