d4b272b8b2527fdac9dbb9dde7ebc9ae88426129
Allan Blackmar <allan.blackmar@gmail.com>
2023-06-17 14:15:42 -0400

    habitz-2: clean up App.js

--- App.js
+++ App.js
@@ -1,41 +1,20 @@
-import React, { useState } from 'react';
-import {  Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 import HabitList from './components/HabitList';
 import styles from './styles';
-import * as HabitActions from './actions/HabitActions';
+import {
+    addHabit,
+    removeHabit,
+    editHabit,
+    saveEditedHabit,
+    markHabitCompleted,
+} from './actions/HabitActions';
 
 export default function App() {
     const [habits, setHabits] = useState([]);
     const [newHabit, setNewHabit] = useState('');
     const [editingHabitId, setEditingHabitId] = useState(null);
     const [editedHabitName, setEditedHabitName] = useState('');
-
-    const addHabit = () => {
-        HabitActions.addHabit(habits, setHabits, newHabit, setNewHabit);
-    };
-
-    const removeHabit = (habitId) => {
-        HabitActions.removeHabit(habits, setHabits, habitId);
-    };
-
-    const editHabit = (habitId) => {
-        HabitActions.editHabit(habits, setHabits, setEditingHabitId, setEditedHabitName, habitId);
-    };
-
-    const saveEditedHabit = () => {
-        HabitActions.saveEditedHabit(
-            habits,
-            setHabits,
-            setEditingHabitId,
-            setEditedHabitName,
-            editingHabitId,
-            editedHabitName
-        );
-    };
-
-    const markHabitCompleted = (habitId) => {
-        HabitActions.markHabitCompleted(habits, setHabits, habitId);
-    };
 
     return (
         <View style={styles.container}>
@@ -48,17 +27,34 @@ export default function App() {
                     value={newHabit}
                     onChangeText={setNewHabit}
                 />
-                <TouchableOpacity style={styles.addButton} onPress={addHabit}>
+                <TouchableOpacity style={styles.addButton} onPress={() => {
+                    addHabit(habits, setHabits, newHabit, setNewHabit);
+                }}>
                     <Text style={styles.addButtonLabel}>Add</Text>
                 </TouchableOpacity>
             </View>
 
             <HabitList
                 habits={habits}
-                onEdit={editHabit}
-                onRemove={removeHabit}
-                onToggleComplete={markHabitCompleted}
-                onEditSave={saveEditedHabit}
+                onEdit={(habitId) => {
+                    editHabit(habits, setHabits, setEditingHabitId, setEditedHabitName, habitId);
+                }}
+                onRemove={(habitId) => {
+                    removeHabit(habits, setHabits, habitId);
+                }}
+                onToggleComplete={(habitId) => {
+                    markHabitCompleted(habits, setHabits, habitId);
+                }}
+                onEditSave={() => {
+                    saveEditedHabit(
+                        habits,
+                        setHabits,
+                        setEditingHabitId,
+                        setEditedHabitName,
+                        editingHabitId,
+                        editedHabitName
+                    );
+                }}
                 editingHabitId={editingHabitId}
                 editedHabitName={editedHabitName}
                 setEditedHabitName={setEditedHabitName}
